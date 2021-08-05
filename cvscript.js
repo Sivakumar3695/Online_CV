925ccb72f60b56b8da245934c3456094ce983ec2
Sivakumar <sivakumar.dpalanisamy@gmail.com>
2021-08-05 15:18:53 +0530

    Initial Commit

---
+++ cvscript.js
@@ -0,0 +1,24 @@
+function showModal(id) {
+	    	let el = document.getElementById(id);  // can also use a query selector
+	    	let modal = document.getElementById('modal-space');
+		modal.getElementsByClassName('modal-header-content')[0].innerText = el.getElementsByClassName('project-name')[0].innerText;		
+		modal.getElementsByClassName('modal-content')[0].innerHTML = el.getElementsByClassName('project-description')[0].innerHTML;
+		if (el.getElementsByClassName('github-a')[0] && el.getElementsByClassName('github-a')[0].href){
+			modal.getElementsByClassName('footer-github-a')[0].href = el.getElementsByClassName('github-a')[0].href;
+			modal.getElementsByClassName('footer-github-a')[0].classList.add('show-github');	   
+			modal.getElementsByClassName('footer-github-a')[0].classList.remove('hide-github');	   
+		}
+		else{
+			modal.getElementsByClassName('footer-github-a')[0].classList.remove('show-github');
+			modal.getElementsByClassName('footer-github-a')[0].classList.add('hide-github');
+		}
+		modal.classList.add('open');
+	    	let bg = document.getElementsByClassName('container-fluid')[0];
+	    	bg.classList.add('wrapper');
+	}
+function hideModal(id) {
+	    let el = document.getElementById(id);  // can also use a query selector
+	    el.classList.remove('open');
+	    let bg = document.getElementsByClassName('container-fluid')[0];
+	    bg.classList.remove('wrapper');
+	}
